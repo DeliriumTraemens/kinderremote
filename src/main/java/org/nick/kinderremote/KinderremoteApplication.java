df53bfbb715268aebe195b9cfad5c98e78a5ad30
Grigorich <oskinnick@gmail.com>
2022-08-07 20:38:25 +0300

    Initial commit

---
+++ src/main/java/org/nick/kinderremote/KinderremoteApplication.java
@@ -0,0 +1,13 @@
+package org.nick.kinderremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinderremoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinderremoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nick/kinderremote/KinderremoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nick.kinderremote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinderremoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
