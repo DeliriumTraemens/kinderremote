a6fef660e2b5eae204f86ef79a159523b2bb0839
Grigorich <oskinnick@gmail.com>
2022-09-22 17:12:31 +0300

    Second Stage Started Spring Data JDBC JPA Entities deactivated Repo Deactivated

--- src/main/java/org/nick/kinderremote/service/CatService.java
+++ src/main/java/org/nick/kinderremote/service/CatService.java
@@ -1,51 +1,51 @@
-package org.nick.kinderremote.service;
-
-import org.nick.kinderremote.data.dto.HtRequest;
-import org.nick.kinderremote.repository.CatalogRepo;
-import org.nick.kinderremote.util.abstractInheritance.ServiceAbstract;
-import org.nick.kinderremote.util.repoServiceUtil.RepoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CatService extends ServiceAbstract implements RepoService {
-
-    private final CatalogRepo catRepo;
-
-    @Autowired
-    public CatService(CatalogRepo catRepo) {
-        this.catRepo = catRepo;
-    }
-
-
-
-    @Override
-    public String getAll(HtRequest request) {
-        return "Got!!! org.nick.kinderremote.service.CatService.getAll Method received Fuck!!!";
-    }
-
-    @Override
-    public String getById(HtRequest request) {
-        return "Youuuu! The CatId "+request.getСatId()+" will be replaced";
-    }
-
-    @Override
-    public String editHierarchy(HtRequest request) {
-        return "The Hierarchy will be replaced!!!";
-    }
-
-    @Override
-    public String editById(HtRequest request) {
-        return null;
-    }
-
-    @Override
-    public String delete(HtRequest request) {
-        return null;
-    }
-
-    @Override
-    public String createNew(HtRequest request) {
-        return null;
-    }
-}
+//package org.nick.kinderremote.service;
+//
+//import org.nick.kinderremote.data.dto.HtRequest;
+//import org.nick.kinderremote.repository.CatalogRepo;
+//import org.nick.kinderremote.util.abstractInheritance.ServiceAbstract;
+//import org.nick.kinderremote.util.repoServiceUtil.RepoService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CatService extends ServiceAbstract implements RepoService {
+//
+//    private final CatalogRepo catRepo;
+//
+//    @Autowired
+//    public CatService(CatalogRepo catRepo) {
+//        this.catRepo = catRepo;
+//    }
+//
+//
+//
+//    @Override
+//    public String getAll(HtRequest request) {
+//        return "Got!!! org.nick.kinderremote.service.CatService.getAll Method received Fuck!!!";
+//    }
+//
+//    @Override
+//    public String getById(HtRequest request) {
+//        return "Youuuu! The CatId "+request.getСatId()+" will be replaced";
+//    }
+//
+//    @Override
+//    public String editHierarchy(HtRequest request) {
+//        return "The Hierarchy will be replaced!!!";
+//    }
+//
+//    @Override
+//    public String editById(HtRequest request) {
+//        return null;
+//    }
+//
+//    @Override
+//    public String delete(HtRequest request) {
+//        return null;
+//    }
+//
+//    @Override
+//    public String createNew(HtRequest request) {
+//        return null;
+//    }
+//}
--- src/main/java/org/nick/kinderremote/service/CategoryService.java
+++ src/main/java/org/nick/kinderremote/service/CategoryService.java
@@ -1,68 +1,68 @@
-package org.nick.kinderremote.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
-import org.nick.kinderremote.data.entity.Catalog;
-import org.nick.kinderremote.data.projections.CatIdNameViev;
-import org.nick.kinderremote.data.projections.CatalogProjIf;
-import org.nick.kinderremote.repository.CatalogRepo;
-import org.nick.kinderremote.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class CategoryService {
-    private final CategoryRepository catRepo;
-    private final CatalogRepo catalogRepo;
-
-    @Autowired
-    public CategoryService(CategoryRepository catRepo, CatalogRepo catalogRepo) {
-        this.catRepo = catRepo;
-        this.catalogRepo = catalogRepo;
-    }
-
-    public String getAllCatalog() {
-        ObjectMapper mapper = new ObjectMapper();
-        List<CatIdNameViev> catalog = catalogRepo.buildCatalogView();
-
-        String sCatalog = null;
-        try {
-            sCatalog = mapper.writeValueAsString(catalog);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return sCatalog;
-    }
-
-    public Catalog catalog() {
-//        System.out.println(catalogRepo.findAll());
-        return catalogRepo.findById(30L).get();
-    }
-
-
-    @SneakyThrows
-    public String getCatalog2() {
-        ObjectMapper mapper = new ObjectMapper();
-        List<CatalogProjIf> catalog2 = catRepo.bildOtherCatalog();
-        String result = mapper.writeValueAsString(catalog2);
-        return result;
-    }
-
-    @SneakyThrows
-    public String getRazvitie() {
-        ObjectMapper mapper = new ObjectMapper();
-
-        CatalogProjIf razvitie = catRepo.findProjById(1151L);
-        return mapper.writeValueAsString(razvitie);
-    }
-
-    @SneakyThrows
-    public String categoryById(Long id) {
-        CatalogProjIf catById = catRepo.findcatbyid(id);
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(catById);
-    }
-}
+//package org.nick.kinderremote.service;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.SneakyThrows;
+//import org.nick.kinderremote.data.entity.Catalog;
+//import org.nick.kinderremote.data.projections.CatIdNameViev;
+//import org.nick.kinderremote.data.projections.CatalogProjIf;
+//import org.nick.kinderremote.repository.CatalogRepo;
+//import org.nick.kinderremote.repository.CategoryRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class CategoryService {
+//    private final CategoryRepository catRepo;
+//    private final CatalogRepo catalogRepo;
+//
+//    @Autowired
+//    public CategoryService(CategoryRepository catRepo, CatalogRepo catalogRepo) {
+//        this.catRepo = catRepo;
+//        this.catalogRepo = catalogRepo;
+//    }
+//
+//    public String getAllCatalog() {
+//        ObjectMapper mapper = new ObjectMapper();
+//        List<CatIdNameViev> catalog = catalogRepo.buildCatalogView();
+//
+//        String sCatalog = null;
+//        try {
+//            sCatalog = mapper.writeValueAsString(catalog);
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//        return sCatalog;
+//    }
+//
+//    public Catalog catalog() {
+////        System.out.println(catalogRepo.findAll());
+//        return catalogRepo.findById(30L).get();
+//    }
+//
+//
+//    @SneakyThrows
+//    public String getCatalog2() {
+//        ObjectMapper mapper = new ObjectMapper();
+//        List<CatalogProjIf> catalog2 = catRepo.bildOtherCatalog();
+//        String result = mapper.writeValueAsString(catalog2);
+//        return result;
+//    }
+//
+//    @SneakyThrows
+//    public String getRazvitie() {
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        CatalogProjIf razvitie = catRepo.findProjById(1151L);
+//        return mapper.writeValueAsString(razvitie);
+//    }
+//
+//    @SneakyThrows
+//    public String categoryById(Long id) {
+//        CatalogProjIf catById = catRepo.findcatbyid(id);
+//        ObjectMapper mapper = new ObjectMapper();
+//        return mapper.writeValueAsString(catById);
+//    }
+//}
