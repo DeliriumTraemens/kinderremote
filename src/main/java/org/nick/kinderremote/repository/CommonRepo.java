cbb30b99ad05988f682952a4c89c5b568af854e0
Grigorich <oskinnick@gmail.com>
2022-09-22 16:46:22 +0300

    Second Stage Started Spring Data JDBC JPA Entities deactivated

--- src/main/java/org/nick/kinderremote/repository/CommonRepo.java
+++ src/main/java/org/nick/kinderremote/repository/CommonRepo.java
@@ -1,11 +1,11 @@
-package org.nick.kinderremote.repository;
-
-import org.nick.kinderremote.data.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommonRepo extends JpaRepository<Category, Long> {
-
-
-}
+//package org.nick.kinderremote.repository;
+//
+//import org.nick.kinderremote.data.entity.Category;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface CommonRepo extends JpaRepository<Category, Long> {
+//
+//
+//}
